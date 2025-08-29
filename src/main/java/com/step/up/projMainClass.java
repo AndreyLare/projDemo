4c78e447eded30e0e500ca761edad44a0af8c3ab
Andre Lare <aalarin@vtb.ru>
2025-08-29 10:23:56 +0300

    seccond commit

--- src/main/java/com/step/up/projMainClass.java
+++ src/main/java/com/step/up/projMainClass.java
@@ -3,5 +3,6 @@ package com.step.up;
 public class projMainClass {
     public static void main(String[] args){
         System.out.println("Hello worlds!!");
+        System.out.println("Hello worlds!!");
     }
 }
