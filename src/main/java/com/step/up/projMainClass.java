a9be7b9b3877349ebd9b412317c388e0cf1a052f
Andre Lare <aalarin@vtb.ru>
2025-08-29 13:22:06 +0300

    another commit

--- src/main/java/com/step/up/projMainClass.java
+++ src/main/java/com/step/up/projMainClass.java
@@ -8,5 +8,9 @@ public class projMainClass {
         System.out.println("Hello worlds!!");
         System.out.println("Hello worlds!!");
         System.out.println("Hello!");
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
 }
