5e4db7571ccb9f417f91c650b18a93574a875efc
Andre Lare <aalarin@vtb.ru>
2025-08-29 11:05:42 +0300

    change logic and added class box

---
+++ src/main/java/com/step/up/box.java
@@ -0,0 +1,4 @@
+package com.step.up;
+
+public class box {
+}
--- src/main/java/com/step/up/projMainClass.java
+++ src/main/java/com/step/up/projMainClass.java
@@ -4,5 +4,6 @@ public class projMainClass {
     public static void main(String[] args){
         System.out.println("Hello worlds!!");
         System.out.println("Hello worlds!!");
+        System.out.println("Hello!");
     }
 }
